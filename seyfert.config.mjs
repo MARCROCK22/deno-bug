e579e8993a1fd0cfce1fb6708fa2e072695cd277
MARCROCK22 <marcos22dev@gmail.com>
2024-10-11 00:06:38 +0000

    a

--- seyfert.config.mjs
+++ seyfert.config.mjs
@@ -1,5 +1,7 @@
 import { config } from 'seyfert';
+// if you comment this, the code executes normally
 import 'jsr:@std/dotenv/load';
+// works in v1.46.3
 
 export default config.bot({
     locations: {
